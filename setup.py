861d776ae2a6de8cb56974a2785a5e3ce2bd5beb
Safa Mert Akmese <safa_mert.akmese@uochb.cas.cz>
2026-03-14 11:19:40 +0100

    rename new script

--- setup.py
+++ setup.py
@@ -16,7 +16,9 @@ setup(
     packages=find_packages(),
     entry_points={
         "console_scripts": [
-            "prepare_data=enzymeexplorer.src.data_preparation.prepare_dataset:main",
+            "get_mmseqs_based_clusters=enzymeexplorer.src.data_preparation.get_mmseqs_based_clusters:main",
+            "get_uniprot_sample=enzymeexplorer.src.data_preparation.get_uniprot_sample:main",
+            "get_phylogeny_based_clusters=enzymeexplorer.src.data_preparation.get_phylogeny_based_clusters:main",
             "gather_plm_embeddings=enzymeexplorer.src.embeddings_extraction.gather_required_embs:main",
             "plm_embeddings=enzymeexplorer.src.embeddings_extraction.transformer_embs:main",
             "enzyme_explorer_main=enzymeexplorer.src.enzyme_explorer_main:main",
