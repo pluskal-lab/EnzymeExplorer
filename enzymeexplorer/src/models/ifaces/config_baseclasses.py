93dd36c92f1d525fd5dcd5d23dd0422411594a7c
Safa Mert Akmese <safa_mert.akmese@uochb.cas.cz>
2026-05-10 01:40:40 +0200

    Disable distractors by default

--- enzymeexplorer/src/models/ifaces/config_baseclasses.py
+++ enzymeexplorer/src/models/ifaces/config_baseclasses.py
@@ -32,7 +32,7 @@ class BaseConfig:
     load_per_class_params_from: str
     reuse_existing_partial_results: bool
     run_against_wetlab: bool = False
-    enable_distractors: bool = True
+    enable_distractors: bool = False
     seq_col_name: str = "Aminoacid_sequence"
 
     @classmethod
